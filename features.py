d9a2b71e745fb3f95e559f4a7183ce274b03d8b3
Andreas Walz <andreas.walz@hs-offenburg.de>
2018-02-14 08:32:25 +0100

    changed license

--- features.py
+++ features.py
@@ -1,31 +1,35 @@
-#!/usr/bin/python
-
-# THE-MAN-TOOLS: A suite of useful tools for cryptography and embedded 
-# system security.
-# Copyright (C) 2015 Andreas Walz
+# Copyright (C) 2017
+# Andreas Walz [andreas.walz@hs-offenburg.de]
+# Offenburg University of Applied Sciences
+# Institute of Reliable Embedded Systems and Communications Electronics (ivESK)
+# [https://ivesk.hs-offenburg.de/]
+# All rights reserved.
 # 
-# Author: Andreas Walz
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
 # 
-# This file is part of THE-MAN-TOOLS.
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
 # 
-# THE-MAN-TOOLS are free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 3 of
-# the License, or (at your option) any later version.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
 # 
-# THE-MAN-TOOLS are distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
 # 
-# You should have received a copy of the GNU General Public License
-# along with THE-MAN-TOOLS; if not, see <http://www.gnu.org/licenses/>
-# or write to the Free Software Foundation, Inc., 51 Franklin Street,
-# Fifth Floor, Boston, MA 02110-1301, USA.
-
-#
-# [parse] -> [check] -> [canonicalize] -> [disentangle] -> [check] -> [...]
-#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 import sys
 
